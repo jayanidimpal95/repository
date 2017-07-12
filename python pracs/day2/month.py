dd56ce9017b18beb79d5f6a292dbe6581da1ad1e
dimpal <dimpal.jayani@quantiphi.com>
2017-07-12 13:02:42 +0530

    final version month

--- python pracs/day2/month.py
+++ python pracs/day2/month.py
@@ -1,6 +1,10 @@
-m = raw_input("Enter Month:")
-
 month=['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
+week=['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']
+m = raw_input("Enter Month:")
+m=m.capitalize()
+w = raw_input("Enter week:")
+w=w.capitalize()
+print "month: ",month.index(m)+1,
+print "week: ",week.index(w)+1 
 
 
-print "sjkahs"

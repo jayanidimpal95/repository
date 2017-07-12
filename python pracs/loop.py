1390ee97cb7ecb845b1b9e3bd79a45eaadc62236
dimpal <dimpal.jayani@quantiphi.com>
2017-07-12 10:26:07 +0530

    day1 pracs

--- python pracs/loop.py
+++ python pracs/loop.py
@@ -1,27 +1,8 @@
-n = raw_input("Enter password:")
-l=len(n)
-l1=list(n)
-f=0
-b=0
-s=0
-sp=0
-d=0
-if(l>=6 and l<=16):
-    f=1
-else:
-    f=0
+i=0
+n = int(raw_input())
 
-for x in  l1:
-    if(x>='A' and x<='Z'):
-        b=b+1
-    if(x>='a' and x<='z'):
-        s=s+1
-    if(x=='$' or x=='#' or x=='@'):
-        sp=sp+1
-    if(x>='0' and x<='9'):
-        d=d+1
-if(f and b and s and sp and d):
-    print "valid password: ",n
-else:
-    print "not a valid password"
-#print f,b,s,sp,d
+a = raw_input()
+l=a.split(" ")
+t=tuple(int(l))
+   
+print hash(t),t
